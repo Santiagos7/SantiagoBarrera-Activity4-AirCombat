9f5b96ea3ffa5920f33de433863e8e9360ffa57d
Santiago Barrera <santiagodsbg@gmail.com>
2018-10-06 10:27:40 -0500

    no message

---
+++ SantiagoBarrera-AirCombat/Assets/Scripts/FighterMotion.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterMotion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate( 0, 0, 1);
+
+		//si presiono tecla izquierda se mueve hacia la izquierda
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			this.transform.Translate(Vector3.left);
+		}
+
+		//si presiono tecla arriba se mueve hacia arriba
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			this.transform.Translate(Vector3.up);
+		}
+
+		//si presiono tecla derecha se mueve hacia la derecha
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			this.transform.Translate(Vector3.right);
+		}
+
+		//si presiono tecla abajo se mueve hacia abajo
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			this.transform.Translate(Vector3.down);
+		}
+
+		//si space disparo
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+
+			GameObject newMissile =  GameObject.Instantiate(Resources.Load("Bullets/Missile") as GameObject);
+				
+			GameObject ShootPivot = GameObject.Find("Fighter/ShootPivot");
+
+			newMissile.transform.SetParent (ShootPivot.transform);
+
+			newMissile.transform.localPosition = Vector3.zero;
+
+			newMissile.transform.SetParent (null);
+
+		}	
+
+			}
+}
---
+++ SantiagoBarrera-AirCombat/Assets/Scripts/MissileMotion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileMotion : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+
+		//Hacia adelante
+		//si space disparo
+				this.transform.Translate(Vector3.up * speed);
+		}	
+			
+
+	void OnCollisionEnter(Collision collision)
+
+	{
+		Debug.Log ("Colision detectada");
+
+		//Buscamos objeto Enemy
+
+		GameObject enemy = collision.collider.gameObject;
+
+		//Eliminamos gameobject encontrado
+		GameObject.Destroy(enemy);
+
+		//Eliminamos tambien el misil
+		GameObject missile = this.gameObject;
+		GameObject.Destroy(missile);
+	}
+
+	} 
